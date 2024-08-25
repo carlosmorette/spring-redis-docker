8189b2a5a9597b487931d7d19699ae300fedbb40
carlosmorette <carlos.morette@outlook.com>
2024-08-24 22:00:13 -0300

    initiate config

---
+++ src/main/java/com/churros/domain/User.java
@@ -0,0 +1,8 @@
+package com.churros.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+  
+}
--- src/main/java/com/churros/dto/SetDTO.java
+++ src/main/java/com/churros/dto/SetDTO.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SetDTO {
+  // TODO: not blank
   private String key;
   private String value;
 }
